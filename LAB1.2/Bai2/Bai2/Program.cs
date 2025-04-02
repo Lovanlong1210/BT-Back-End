bdd38db93f1847f857add903a95049ef8f04279a
Lo Van Long <nguga59@gmail.com>
2025-04-02 14:06:36 +0700

    first commit

---
+++ LAB1.2/Bai2/Bai2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class Program
+{
+    static bool IsPrime(int number)
+    {
+        if (number <= 1) return false;
+        if (number == 2) return true;
+        if (number % 2 == 0) return false;
+
+        for (int i = 3; i * i <= number; i += 2)
+        {
+            if (number % i == 0)
+                return false;
+        }
+        return true;
+    }
+
+    static void Main()
+    {
+        Console.Write("Nhap so phan tu cua mang: ");
+        int n = int.Parse(Console.ReadLine());
+        int[] arr = new int[n];
+
+        Console.WriteLine("Nhap cac phan tu cua mang:");
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write($"Phan tu thu {i + 1}: ");
+            arr[i] = int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine("Cac so nguyen to trong mang:");
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (IsPrime(arr[i]))
+            {
+                Console.WriteLine($"chi so: {i}, Gia tri: {arr[i]}");
+            }
+        }
+    }
+}
